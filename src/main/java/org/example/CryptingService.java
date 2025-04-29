1d57733091e950d5542c93acd296494b51d1662f
KvyacheS <kvyaches@gmail.com>
2025-04-29 09:54:57 +0300

    fix bugs, refactor, add backlog

--- src/main/java/org/example/CryptingService.java
+++ src/main/java/org/example/CryptingService.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class CryptingService {
 
-    public CryptingService (char[] alphabet,int shift, boolean skipMissedAlphabet) {
+    public CryptingService(char[] alphabet, int shift, boolean skipMissedAlphabet) {
         this.alphabet = alphabet;
-        this.shift = shift % alphabet.length;//смысла нет
+        this.shift = shift % alphabet.length;//Сдвиг не может быть больше  или равен длинне алфавита- результат будет аналогичен
         this.skipMissedAlphabet = skipMissedAlphabet;
     }
 
-    private static final char[] SYSCHARS = {'\n', '\r'};
+    private static final char[] SYSCHARS = {'\n', '\r', '\t', '\b', '\f'};
 
     private final char[] alphabet;
 
@@ -17,7 +17,7 @@ public class CryptingService {
     private final boolean skipMissedAlphabet;
 
     public void setShift(int shift) {
-        this.shift = shift;
+        this.shift = shift % alphabet.length;
     }
 
     public char[] encrypt(char[] inputChars, int effectiveLength) throws CryptingException {
@@ -36,6 +36,16 @@ public class CryptingService {
         return crypt(inputChars, -shift, inputChars.length);
     }
 
+    /**
+     * Шифрующюший-Дешифрующий метод
+     * @param inputChars Массив символов, которые  необходимо зашифровать
+     * @param shift Сдвиг(ключ) на который  нужно преобразовать переданный массив
+     * @param effectiveLength (фактический размер символов, которые нужно шифровать)
+     * @return зашифрованный/расшифрованный массив символов  inputChars
+     * @throws CryptingException если не выключен пропуск не найденных в алфавите символов, кидает исключение
+     * если символа нет в алфавите
+     */
+    //TODO: Попробовать сделать что-нибудь, чтобы не было необходимости вычислять эффиктивную длинну массива
     private char[] crypt(char[] inputChars, int shift, int effectiveLength) throws CryptingException {
         char[] result = new char[effectiveLength];
         for (int idx = 0; idx < effectiveLength; idx++) {
@@ -45,6 +55,7 @@ public class CryptingService {
             }
             int charIdx = findAlphaBetPos(inputChars[idx], alphabet);
             if (charIdx == -1) {
+                //В случае, если включено жесткое соотвествие алфавиту
                 if (!skipMissedAlphabet) {
                     throw new CryptingException(String.format("Symbol %s not exist in alphabet", inputChars[idx]));
                 }
@@ -57,6 +68,12 @@ public class CryptingService {
         return result;
     }
 
+    /**
+     * Поиск символа в алфавите
+     * @param inputChar искомый символ
+     * @param alphabet алфавит, в котором ведется поиск
+     * @return позицю в алфавите или  -1, если символа там нет
+     */
     public int findAlphaBetPos(char inputChar, char[] alphabet) {
         int pos = 0;
         while (pos < alphabet.length && alphabet[pos] != inputChar) {
@@ -65,6 +82,11 @@ public class CryptingService {
         return pos < alphabet.length ? pos : -1;
     }
 
+    /**
+     * Проверка на то, что символ относится к управляющей последовательности
+     * @param ch переданный на проверку символ
+     * @return true, если символ является управляющим
+     */
     public boolean isSysChar(char ch) {
         boolean result = false;
         int idx = 0;
